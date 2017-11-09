886eb021b5b6373587c6c8a201ba408ab63080ae
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2017-11-09 17:09:31 +0200

    Removed something in pthread.

--- 01.w_Defects/pthread.h
+++ 01.w_Defects/pthread.h
@@ -9,9 +9,9 @@ typedef int pthread_mutexattr_t;
 
 int PTHREAD_MUTEX_INITIALIZER;
 
-int pthread_create(pthread_t *restrict thread,
-       const pthread_attr_t *restrict attr,
-       void *(*start_routine)(void*), void *restrict arg);
+int pthread_create(pthread_t * thread,
+       const pthread_attr_t * attr,
+       void *(*start_routine)(void*), void *arg);
 
 int pthread_mutex_lock(pthread_mutex_t *mutex);
 int pthread_mutex_trylock(pthread_mutex_t *mutex);
